795674945639410af2615a84a57f1abce0cff4f1
Jinhang Jiang <jinhangjiang@gmail.com>
2025-02-22 17:22:30 -0600

    v1.1.1

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="textregress",
-    version="1.1.0",
+    version="1.1.1",
     description="A package for performing advanced regression on text data using unified deep learning framework.",
-    author="Jinhang Jiang, Weiyao Peng, Karthik Srinivasan",
+    author="Jinhang Jiang, Ben Liu, Weiyao Peng, Karthik Srinivasan",
     author_email="jinhang@asu.edu",
     packages=find_packages(),
     install_requires=[
